a77b6bd1f27e98ce4af195cbbb149baec23224ad
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2024-12-11 16:56:45 +0100

    Appunti Typescript

---
+++ index.js
@@ -0,0 +1,32 @@
+// Appunti e risposte
+
+// Spiegazione Generica di TypeScript
+//    E' Un linguaggio di programmazione open source e si tratta di un super-set di JavaScript sviluppato da Microsoft.
+//    Permette di scrivere applicazioni più robuste e mantenibili grazie alla tipizzazione statica.
+//    Gestisce i tipi delle variabili in fase di sviluppo, evitando errori durante l'esecuzione.
+
+// Differenze con JavaScript:
+// o	Tipizzazione statica: In TypeScript si definiscono i tipi delle variabili (es. string, number).
+// o	Sintassi aggiuntiva: Supporta funzionalità come interfacce, generics.
+// o	Compilazione: TypeScript deve essere compilato in JavaScript per essere eseguito.
+
+// Il Compilatore TS
+//    Trasforma il codice TypeScript in JavaScript eseguibile. Aiuta anche a trovare errori durante la compilazione.
+// Come si usa:
+// 1.	Installazione: npm install -g typescript.
+// 2.	Compilazione: tsc nomefile.ts per generare un file .js.
+// 3.	Configurazione: Uso del file tsconfig.json per personalizzare la compilazione.
+
+// La Type Inference
+// Permette al compilatore di dedurre automaticamente il tipo delle variabili.
+
+// Il Tipo ‘any’
+//  Disabilita la tipizzazione statica, permettendo a una variabile di assumere qualsiasi tipo.
+
+// Il Tipo Union
+// Permette a una variabile di avere più di un tipo.
+
+// Le Interfaces in TypeScript
+// Definiscono la struttura di un oggetto, specificando le proprietà e i metodi necessari.
+
+// I Generics: Permettono di scrivere funzioni, classi e interfacce generiche che funzionano con qualsiasi tipo.
